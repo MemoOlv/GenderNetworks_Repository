f74b76dfb99924be629f129b393a6cde5df5481e
MemoOlv <guolg@ier.unam.mx>
2022-07-10 00:35:56 -0500

    🎨 Make format

--- tests/test_data_cleaning.py
+++ tests/test_data_cleaning.py
@@ -11,7 +11,8 @@ def test_read_data():
 def test_read_tables():
     hogar_path = "tests/data/hogares_data.csv"
     obtained_output = read_tables(hogar_path)
-    expected_output = pd.DataFrame(data = {"folioviv": [100003801, 100003802], "foliohog": [np.nan, np.nan], "huespedes": [0, 0]})
-    expected_output.set_index("folioviv", inplace = True)
+    expected_output = pd.DataFrame(
+        data={"folioviv": [100003801, 100003802], "foliohog": [np.nan, np.nan], "huespedes": [0, 0]}
+    )
+    expected_output.set_index("folioviv", inplace=True)
     pd.testing.assert_frame_equal(obtained_output, expected_output)
-    
