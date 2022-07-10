2741cba2b3532eac8e0c7ae8a35577b5605ef1e5
MemoOlv <guolg@ier.unam.mx>
2022-07-10 00:17:13 -0500

    ✅ Pass test

--- gender_energy_networks/data_processing_functions.py
+++ gender_energy_networks/data_processing_functions.py
@@ -6,7 +6,7 @@ from scipy.stats import multivariate_normal
 
 
 def read_tables(data_path):
-    return pd.read_csv(data_path)
+    return pd.read_csv(data_path, index_col="folioviv")
 
 def read_data(year, data_path):
     path_name = data_path + "ENIGH" + year + "/"
