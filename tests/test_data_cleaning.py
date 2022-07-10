f676773a309b5aefef337c1405cac4f71af637ec
MemoOlv <guolg@ier.unam.mx>
2022-07-10 00:23:43 -0500

    🧪 Expect nan values from &

--- tests/test_data_cleaning.py
+++ tests/test_data_cleaning.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from gender_energy_networks import read_data, read_tables
 
 
@@ -10,7 +11,7 @@ def test_read_data():
 def test_read_tables():
     hogar_path = "tests/data/hogares_data.csv"
     obtained_output = read_tables(hogar_path)
-    expected_output = pd.DataFrame(data = {"folioviv": [100003801, 100003802], "foliohog": [1, 1], "huespedes": [0, 0]})
+    expected_output = pd.DataFrame(data = {"folioviv": [100003801, 100003802], "foliohog": [np.nan, 1], "huespedes": [0, 0]})
     expected_output.set_index("folioviv", inplace = True)
     pd.testing.assert_frame_equal(obtained_output, expected_output)
     
