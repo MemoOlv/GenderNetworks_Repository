8c6fcfae587ff1a16eb9c4682f9ae946fdf32d0a
MemoOlv <guolg@ier.unam.mx>
2022-07-10 21:18:44 -0500

    ♻️ Rename  path name

--- tests/test_data_cleaning.py
+++ tests/test_data_cleaning.py
@@ -9,13 +9,14 @@ def test_read_data():
 
 
 def test_read_tables():
-    hogar_path = "tests/data/example_data.csv"
-    obtained_output = read_tables(hogar_path)
+    example_path = "tests/data/example_data.csv"
+    obtained_output = read_tables(example_path)
     expected_output = pd.DataFrame(
         data={"folioviv": [100003801, 100003802], "foliohog": [np.nan, np.nan], "huespedes": [0, 0]}
     )
     expected_output.set_index("folioviv", inplace=True)
     pd.testing.assert_frame_equal(obtained_output, expected_output)
+
 
 
 def test_merge_data():
