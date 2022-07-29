ca04ecc3ea41dc67e7e62612e2f336524ecf19cf
MemoOlv <guolg@ier.unam.mx>
2022-07-28 22:20:35 -0500

    🎨 Make format

--- tests/test_get_dataframes_from_raw_data.py
+++ tests/test_get_dataframes_from_raw_data.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import numpy as np
-from gender_energy_networks import read_tables, merge_data, get_enigh_2016_dataframe, get_enigh_2018_and_2020_dataframe
+from gender_energy_networks import (
+    read_tables,
+    merge_data,
+    get_enigh_2016_dataframe,
+    get_enigh_2018_and_2020_dataframe,
+)
 
 
 def test_get_enigh_2016_dataframe():
@@ -26,6 +31,7 @@ def test_get_enigh_2018_and_2020_dataframe():
     expected_columns_length = 487
     assert obtained_columns_length == expected_columns_length
 
+
 def test_read_tables():
     example_path = "tests/data/example_data.csv"
     obtained_output = read_tables(example_path)
@@ -34,10 +40,11 @@ def test_read_tables():
     )
     expected_output.set_index("folioviv", inplace=True)
     pd.testing.assert_frame_equal(obtained_output, expected_output)
-    
+
     expected_output = pd.DataFrame(
         data={"folioviv": [100003801, 100003802], "foliohog": [np.nan, np.nan], "huespedes": [0, 0]}
     )
+
 
 def test_merge_data():
     hogar_path = "tests/data/hogares_data.csv"
@@ -55,7 +62,7 @@ def test_merge_data():
             "foliohog_x": [1, 1],
             "acc_alim1": [1, 2],
             "foliohog_y": [1, 1],
-            "edad_jefe": [33, 29]
+            "edad_jefe": [33, 29],
         }
     )
     obtained_merged_dataframe.set_index("folioviv", inplace=True)
