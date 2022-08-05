f4d156e156cd4a8f0295c1441d38fc7cb8023925
MemoOlv <guolg@ier.unam.mx>
2022-08-04 20:37:38 -0500

    ✅ Check for condor importation

--- tests/test_enigh_database_class.py
+++ tests/test_enigh_database_class.py
@@ -1,3 +1,5 @@
+from condor import condor_object
+
 from gender_energy_networks import ENIGH_database
 
 def test_ENIGH_database_set_year():
@@ -6,3 +8,9 @@ def test_ENIGH_database_set_year():
     ENIGH.set_year(expected_year_of_analisys)
     obtained_year_of_analisys = ENIGH.year
     assert expected_year_of_analisys == obtained_year_of_analisys
+
+def test_condor_installation():
+    class ThisClass: pass
+    obtained_condor_object_type = type(condor_object)
+    expected_condor_object_type = type(ThisClass)
+    assert obtained_condor_object_type == expected_condor_object_type
