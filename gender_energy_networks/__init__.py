22fed2c590d1f07be879e9789d6bb7de112874c4
MemoOlv <guolg@ier.unam.mx>
2022-08-04 19:58:25 -0500

    ✅ Pass test

--- gender_energy_networks/__init__.py
+++ gender_energy_networks/__init__.py
@@ -4,3 +4,4 @@ __version__ = "0.1.0"
 from .enigh_class import *  # noqa
 from .data_processing_functions import *  # noqa
 from .get_dataframes_from_raw_data import *  # noqa
+from .enigh_database_class import *  # noqa
