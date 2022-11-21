0953bd57b7a0b63eba537ea533ffd2cc2ec5f7b6
MemoOlv <guolg@ier.unam.mx>
2022-11-21 15:36:11 -0600

    ✅ Add data into tests

--- tests/test_hash_figures.py
+++ tests/test_hash_figures.py
@@ -5,7 +5,7 @@ from gender_energy_networks import plot_null_values
 
 def test_plot_null_values():
     enigh_dataframe = pd.read_csv("tests/data/input_plot_null_values.csv")
-    file_path = "tests/figures/plot_null_values.png"
+    file_path = "tests/data/plot_null_values.png"
     plot_null_values(enigh_dataframe, file_path)
     obtained_hash = _get_hash_from_file(file_path)
     expected_hash = "0684c592f94501ee3e1d8f1e4bd9c6f5"
