6caac776d65845be60b4abb9f689ffae26b3e58c
MemoOlv <guolg@ier.unam.mx>
2022-11-21 15:44:27 -0600

    ♻️ Extract method

--- gender_energy_networks/plot_null_values.py
+++ gender_energy_networks/plot_null_values.py
@@ -5,18 +5,9 @@ import matplotlib.gridspec as gridspec
 
 def plot_null_values(enigh_dataframe, file_path):
     sns.set_style("whitegrid", {"grid.linestyle": "--", "axes.edgecolor": "0"})
-    ENIGH = enigh_dataframe
-
-    pct_missing = []
-    col_missing = []
-    pct_col = []
-    for col in ENIGH.columns:
-        pct = np.mean(ENIGH[col].isnull())
-        pct_missing = np.append(pct_missing, pct)
-        pct_col.append(str(col) + "->" + str(pct) + "%")
-        if pct >= 0.1:
-            col_missing = np.append(col_missing, col)
-
+    
+    percentage_missing = _get_percentage_missing(enigh_dataframe)
+    
     f = plt.figure(figsize=(10, 6))
     f.subplots_adjust(hspace=0.2)
     fontsize_ticks = 20
@@ -24,7 +15,10 @@ def plot_null_values(enigh_dataframe, file_path):
     ax = plt.subplot(gs[0])
     ax.tick_params(labelsize=fontsize_ticks)
     sns.despine()
-    sns.histplot(pct_missing, kde=False)
+    sns.histplot(percentage_missing, kde=False)
     plt.xlabel("$n_{i}$", fontsize=fontsize_ticks)
     plt.ylabel("Frecuencia", fontsize=fontsize_ticks)
     plt.savefig(file_path, bbox_inches="tight")
+
+def _get_percentage_missing(enigh_dataframe):
+    return [np.mean(enigh_dataframe[col].isnull()) for col in enigh_dataframe.columns]
