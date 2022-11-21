b4319dbf2d53761b62418f83d0d0f81f8a2badc1
MemoOlv <guolg@ier.unam.mx>
2022-11-21 13:42:06 -0600

    🧪 Update failing tests

--- tests/test_hash_figures.py
+++ tests/test_hash_figures.py
@@ -1,7 +1,9 @@
 import hashlib
 
+from gender_energy_networks import plot_null_values
+
 def test_plot_null_values():
-    expected_hash = "abc"
+    expected_hash = "5c3642e40c73bc1d0363a68322aef26a"
     file_path = "tests/figures/plot_null_values_2016.pdf"
     plot_null_values(file_path)
     obtained_hash = get_hash_from_file(file_path)
@@ -9,5 +11,5 @@ def test_plot_null_values():
 
 
 def get_hash_from_file(file_path):
-    encoded_string = file_path.enconde("utf-8")
-    return hashlib.md5(encoded_string).hexdigest()
+    encoded_string = hashlib.md5(file_path.encode("utf-8"))
+    return encoded_string.hexdigest()
